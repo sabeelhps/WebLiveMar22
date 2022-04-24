6b5735c89b73a71feb4be54eb2f067ac4dab7474
Sabeel Khan <sabeelhps@gmail.com>
2022-04-24 21:42:00 +0530

    Lecture-13

---
+++ Lecture-13/DecisionMaking/app.js
@@ -0,0 +1,47 @@
+
+
+// let a = 10;
+// let b = 20;
+
+// if (a > b) {
+//     console.log('a is greater');
+// }
+
+// if (b > a) {
+//     console.log('b is greater');
+// }
+
+
+// if (a > b) {
+//     console.log('a is greater');
+// } else {
+//     console.log('b is greater');
+// }
+
+
+// let name=null;
+
+// const fullName = name || 'Anonymous';
+
+// console.log(fullName);
+
+
+
+
+const age = parseInt(prompt("Enter Your Age")); 
+
+// console.log(typeof age);
+
+console.log(age);
+
+if (age < 18) {
+    alert('You cannot enter and drink!');
+}
+else if (age >= 18 && age < 25) {
+    alert("You can enter but cannot drink!");
+}
+else {
+    alert("You can enter and drink");
+}
+
+
---
+++ Lecture-13/StringMethods/app.js
@@ -0,0 +1,24 @@
+
+
+// let msg = "Leave me alone.I hate you plzzz!!!!";
+// const angryMsg = msg.toUpperCase();
+// console.log(angryMsg);
+
+// const firstName = 'Sabeel';
+// const lastName = 'Khan';
+
+
+// // const fullName = "Mr." + lastName + "," + firstName;
+
+// // String template literals
+// const fullName = `Mr.${lastName},${firstName}`;
+
+// console.log(fullName);
+
+
+
+    
+    
+
+
+

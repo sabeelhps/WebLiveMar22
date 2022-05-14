32349601842aa40d13a60e79e0dc7c0dfee42c72
Sabeel Khan <sabeelhps@gmail.com>
2022-05-14 21:35:49 +0530

    Lecture-15

---
+++ Lecture-15/Function-DeepDive/app.js
@@ -0,0 +1,83 @@
+
+
+// console.log(a);
+
+// b();
+
+
+// let a = 200; 
+
+// console.log(a);
+
+
+// function b() {
+//     var c = 777;
+//     console.log(c);
+// }
+
+
+
+
+
+
+
+// -----------------------------
+
+// var a = 200;
+
+// function b() {
+//     var c = 777;
+//     console.log(c);
+
+//     function d() {
+//         console.log("Inside d");
+//     }
+
+//     d();
+// }
+
+// console.log(a);
+
+// b();
+
+
+
+// function fun() { //normal function definition using function keyword
+    
+// }
+
+
+// // Function expression
+// const additon = function add(x,y) {
+//     console.log(x + y);
+// }
+
+// // console.log(additon)
+// additon(5, 3);
+
+
+
+// // Function expression - Using anonymous function
+// const additon = function(x,y) {
+//     console.log(x + y);
+// }
+
+// // console.log(additon)
+// additon(5,3);
+
+
+console.log(a);
+
+var a = 200;
+
+
+cube(3);
+
+var cube = function (num) {
+    return num ** 3;
+}
+
+
+
+
+
---
+++ Lecture-15/Functions/app.js
@@ -0,0 +1,99 @@
+
+
+
+function add() { //function definition
+    const a = 100;
+    const b = 200;
+    const res = a + b;
+    console.log(res);
+}
+
+// ..
+// ..
+// ...
+// ...10000
+
+
+// add() //calling a function
+
+// ...
+// ..500
+
+
+// add() // calling function
+
+
+
+// function fun(a, b, c, d) {
+    
+
+//     console.log(a);
+//     console.log(b);
+//     console.log(c);
+//     console.log(d);
+    
+// }
+
+// fun(200,300,400,500);
+
+
+// function cube(num) {
+    
+//     const res = num ** 3;
+
+//     return res;
+// }
+
+// const ans = cube(4);
+
+// console.log(ans);
+
+
+
+
+// function f(x) {
+    
+//     if (x===5) {
+//         console.log('value of x is 5, hence stoping a function');
+//         return x;
+//     }
+
+//     console.log("After if condition");
+//     console.log("After if condition");
+
+// }
+
+// f(5);
+
+
+// Default arguments
+
+
+
+// function multiply(x, y=3) {
+
+//     return x * y;
+// }
+
+// console.log(multiply(4, 5)); //20
+// console.log(multiply(4)); //12
+// console.log(multiply()); //NaN
+
+
+
+// function divide(a=5, b) {
+    
+//     return a / b;
+// }
+
+// console.log(divide(2));
+
+
+// function addition(a, b, c=10) {
+//     return a + b + c;
+// }
+
+// console.log(addition(1, 2, 3));
+// console.log(addition(1, 2));
+
+

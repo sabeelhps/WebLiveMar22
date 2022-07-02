3e54c654df3bd4645d1ce8e3b5dd2e8fbe3d676d
Sabeel Khan <sabeelhps@gmail.com>
2022-07-02 20:19:51 +0530

    Lecture-30

---
+++ Lecture-30/FileSystem/index.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const path = require('path');
+
+
+// console.log(__dirname);
+// console.log(process.cwd());
+
+const F1 = path.join(__dirname, 'files','ghi.txt');
+
+// fs.readFile('abc.txt', {
+//     encoding: 'utf-8',
+//     flag: 'r'
+// }, (err, data) => {
+//     if (err) throw err;
+//     console.log(data);
+// });
+
+// fs.readFile('abc.txt', (err, data) => {
+//     if (err) throw err;
+//     console.log(data.toString());
+// });
+
+
+// -------------------Writing into a file----
+
+// const data = 'We are writing with node.js';
+
+// fs.writeFile(F1, data, (err) => {
+//     if (err) throw err;
+//     console.log('File Written Successfully');
+// });
+
+
+
+
+
+

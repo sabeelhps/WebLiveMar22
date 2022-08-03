ee557bbe6be8ace231b3588d4bb23d4ccfd8c4be
Sabeel Khan <sabeelhps@gmail.com>
2022-08-03 21:41:41 +0530

    Lecture-37

---
+++ Lecture-37/e-commerce-v1/app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const ejsMate = require('ejs-mate');
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+
+
+
+// Routes 
+const productRoutes = require('./routes/productRoutes');
+
+
+
+mongoose.connect('mongodb://localhost:27017/shopping-app-wb-lv-mar')
+    .then(() => console.log("DB CONNECTED!"))
+    .catch((err) => console.log(err));
+
+app.engine('ejs', ejsMate);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'))
+
+
+
+
+app.use(productRoutes);
+
+
+app.listen(3000,()=>{
+  console.log('server started at port 3000');
+});
